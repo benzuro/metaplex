e5e699481c2785402cabe7a471a4f933d24be6e6
benzuro <98982231+benzuro@users.noreply.github.com>
2022-02-25 19:08:45 -0700

    Updates

--- _next/static/fyAacx25Nks3OhHCwlvKR/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-292552b0e2f4eb088665.js"],"/_error":["static/chunks/pages/_error-077790e38fbfe3891eb2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
